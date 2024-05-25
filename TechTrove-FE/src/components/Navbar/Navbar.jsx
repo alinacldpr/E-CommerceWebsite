d59907b9ecacd350333252f8ff253fe6b99fb683
alinacldpr <alinacldpr@gmail.com>
2024-05-25 21:35:31 +0300

    changed the title in the navbar

--- TechTrove-FE/src/components/Navbar/Navbar.jsx
+++ TechTrove-FE/src/components/Navbar/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = ({ handleOrderPopup }) => {
               href="#"
               className="text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl"
 >
-              Eshop
+              TechTrove
 </a>
             {/* Menu Items */}
 <div className="hidden lg:block">
