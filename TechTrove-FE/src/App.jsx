b9cb55df7d0153e6574c999d3e720cc3225e89cd
alinacldpr <alinacldpr@gmail.com>
2024-05-25 21:29:56 +0300

    added log in button and made some changes in home

--- TechTrove-FE/src/App.jsx
+++ TechTrove-FE/src/App.jsx
@@ -5,7 +5,6 @@ import Category from "./components/Category/Category";
 import Category2 from "./components/Category/Category2";
 import Services from "./components/Services/Services";
 import Banner from "./components/Banner/Banner";
-import Partners from "./components/Partners/Partners.jsx";
 
 import headphone from "./assets/hero/headphone.png";
 import smartwatch2 from "./assets/category/smartwatch2-removebg-preview.png";
@@ -20,24 +19,24 @@ import "aos/dist/aos.css";
 const BannerData = {
   discount: "30% OFF",
   title: "Fine Smile",
-  date: "10 Jan to 28 Jan",
+  date: "10 May to 10 June",
   image: headphone,
   title2: "Air Solo Bass",
   title3: "Winter Sale",
   title4:
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+    "Experience the best sound quality with our Air Solo Bass headphones. Limited time offer, don't miss out!",
   bgColor: "#f42c37",
 };
 
 const BannerData2 = {
   discount: "30% OFF",
   title: "Happy Hours",
-  date: "14 Jan to 28 Jan",
+  date: "20 May to 20 June",
   image: smartwatch2,
   title2: "Smart Solo",
   title3: "Winter Sale",
   title4:
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+    "Stay ahead with our Smart Solo watch. Track your activity and enjoy seamless connectivity. Limited time offer!",
   bgColor: "#2dcc6f",
 };
 
@@ -69,7 +68,6 @@ const App = () => {
       <Products />
       <Banner data={BannerData2} />
       <Blogs />
-      <Partners />
       <Footer />
       <Popup orderPopup={orderPopup} handleOrderPopup={handleOrderPopup} />
     </div>
