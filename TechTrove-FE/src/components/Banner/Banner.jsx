d9de7ada5caabfecd3bda2bd4581fbd22c4de1f1
alinacldpr <alinacldpr@gmail.com>
2024-06-05 23:20:19 +0300

    navigate from Shop By Category and Shop to Shop page

--- TechTrove-FE/src/components/Banner/Banner.jsx
+++ TechTrove-FE/src/components/Banner/Banner.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 const Banner = ({ data }) => {
+  const navigate = useNavigate();
+
+  const handleShopNowClick = () => {
+    navigate('/shop');
+  };
+
   return (
     <div className="min-h-[550px] flex justify-center items-center py-12">
       <div className="container">
@@ -45,6 +52,7 @@ const Banner = ({ data }) => {
             </p>
             <div data-aos="fade-up" data-aos-offset="0">
               <button
+                onClick={handleShopNowClick}
                 style={{ color: data.bgColor }}
                 className="bg-white py-2 px-4 rounded-full"
               >
